ba2f502cc37fa9321f275219b312b63431d8ca3a
dewmini1212 <dissanaykedewmini636@gmail.com>
2025-08-30 13:56:09 +0530

    vima

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.post('/auth', async (req, res) => {
         var {username, password, govRole} = req.body;
         const isValid = await db.login(username, password);
         if (isValid) {
-            res.redirect
-            ({ username, govRole });
+            res.json({ username, govRole} ).redirect('/gov/dashboard')
+      
         } else {
             res.status(401).send('Invalid credentials');
         }
