68ac80eae2c11bcfce517f2f115772b1ce6d5021
omega-u20 <udulaforactiveworlds@gmail.com>
2025-09-02 15:32:05 +0530

    backend

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
-const { MongoClient } = require('mongodb');
-const db_pass = process.env.DB_PASS; 
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+import e from 'express';
+dotenv.config();
 
-async function login(username, password) {
+const db_pass = process.env.DB_PASS;
+
+export async function login(username, password) {
     const uri = `mongodb+srv://prodev:${db_pass}@clusterdev.owm1unr.mongodb.net/?retryWrites=true&w=majority&appName=ClusterDev`;
     const client = new MongoClient(uri);
 
@@ -9,21 +13,21 @@ async function login(username, password) {
         await client.connect();
         const db = client.db('TCC');
         const users = db.collection('Utopia');
-        const user = await users.findOne({ username, password});
-        return true/* user !== null; */ 
+        const user = await users.findOne({ username, password });
+        return true/* user !== null; */;
     } catch (error) {
         console.error('Error during login:', error);
-        return true/* user !== null; */
-    }finally {
+        return true/* user !== null; */;
+    } finally {
         await client.close();
     }
 }
 
-async function getGovInfo(username){
+export async function getGovInfo(username) {
 
 }
 
-async function getCitzInfo(nic){
+export async function getCitzInfo(nic) {
     const uri = `mongodb+srv://prodev:${db_pass}@clusterdev.owm1unr.mongodb.net/?retryWrites=true&w=majority&appName=ClusterDev`;
     const client = new MongoClient(uri);
 
@@ -44,4 +48,26 @@ async function getCitzInfo(nic){
     }
 }
 
-module.exports = { login, getGovInfo, getCitzInfo };
+export async function citzSignup(nic, fullName, email, password, address, phone) {
+    const uri = `mongodb+srv://prodev:${db_pass}@clusterdev.owm1unr.mongodb.net/?retryWrites=true&w=majority&appName=ClusterDev`;
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        const db = client.db('TCC');
+        const citizens = db.collection('Citizens');
+        const existingUser = await citizens.findOne({ nic });
+        if (existingUser) {
+            return { success: false, message: 'User already exists' };
+        }else {
+            await citizens.insertOne({ nic, name: fullName, email, password, address, mobilenumber: phone });
+            return { success: true, message: 'User registered successfully' };
+        }
+    } catch (error) {
+        console.error('Error during signup:', error);
+        return { success: false, message: 'Error during signup' };
+    } finally {
+        await client.close();
+    }
+
+}
